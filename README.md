98901cbadda1fba6294490bb8727bc0d66fb56e0
Sooraj <codewithsooraj@gmail.com>
2022-01-06 17:28:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Change-Multiple-Images-Using-JS
+ 
---
+++ js/main.js
@@ -0,0 +1,32 @@
+function cursorIn1(){
+    document.getElementById("displayimage").src="images/i2.png"
+}
+function cursorIn2(){
+    document.getElementById("displayimage").src="images/i3.png"
+}
+function cursorIn3(){
+    document.getElementById("displayimage").src="images/i4.jpg"
+}
+function cursorIn4(){
+    document.getElementById("displayimage").src="images/i5.png"
+}
+function cursorIn5(){
+    document.getElementById("displayimage").src="images/i6.jpg"
+}
+
+function cursorOut1(){
+    document.getElementById("displayimage").src="images/i1.jpg"
+}
+function cursorOut2(){
+    document.getElementById("displayimage").src="images/i1.jpg"
+}
+function cursorOut3(){
+    document.getElementById("displayimage").src="images/i1.jpg"
+}
+function cursorOut4(){
+    document.getElementById("displayimage").src="images/i1.jpg"
+}
+function cursorOut5(){
+    document.getElementById("displayimage").src="images/i1.jpg"
+}
+
